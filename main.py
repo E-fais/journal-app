8df4d59eddb2346ea8d109bc702b06d77a0b36ab
fais edathil <faiesedathil@gmail.com>
2024-07-29 14:01:18 +0530

    change style

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import ttkbootstrap as ttkb
 from ttkbootstrap import *
 
 
-login_page=ttkb.Window(themename='litera')
-login_page.title('Login Page')
 
-frame=ttkb.Frame(login_page,padding=(50,10),width=440,height=280,bootstyle="light")
+root=ttkb.Window(themename='litera')
+root.title('Quick Journal')
+root.iconbitmap('logo.ico')
+frame=ttkb.LabelFrame(root,padding=(50,10),width=440,height=280,bootstyle="dark")
 frame.pack(padx=50,pady=40)
 
 header=ttkb.Label(frame,text="Quick Journal ",font=('helvetica',16),bootstyle='dark')
@@ -15,8 +16,8 @@ user_name=ttkb.Entry(frame,width=30)
 password_label=ttkb.Label(frame,text='Password : ',bootstyle='dark',font=('helvetica',14))
 password=ttkb.Entry(frame,width=30)
 submit_btn=ttkb.Button(frame,text='Login',width=50,bootstyle='dark')
-signup_btn=ttkb.Button(frame,text='Sign Up',bootstyle='info-link')
-guest_btn=ttkb.Button(frame,text='Login as guest',bootstyle='info-link')
+signup_btn=ttkb.Button(frame,text='Sign Up',bootstyle='link')
+guest_btn=ttkb.Button(frame,text='Login as guest',bootstyle='link')
 
 
 
@@ -30,4 +31,4 @@ submit_btn.grid(row=4,column=0,columnspan=2,pady=5)
 signup_btn.grid(row=5,column=0)
 guest_btn.grid(row=5,column=1)
 
-login_page.mainloop()
+root.mainloop()
